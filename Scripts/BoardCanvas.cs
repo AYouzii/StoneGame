7322a58d59d19afc96d5735e02b6f2666a6540fb
AYouzii <2985468220@qq.com>
2023-06-15 21:42:57 +0800

    update

--- Scripts/BoardCanvas.cs
+++ Scripts/BoardCanvas.cs
@@ -17,35 +17,45 @@ public class BoardCanvas : MonoBehaviour
     private int m_text_index = 0;
     private const int IMAGE_NUM = 4;
     private const int TEXT_NUM = 4;
+
+    public TMP_Text plots; //画布上显示的文本
+    public TextAsset[] plot_text_arr; //剧情文本存放在Assets文件夹下的文本文档里
+    public Button next_button;
+    private int plots_index;
+    private const int MAX_PLOTS_INDEX = 2;
     // Start is called before the first frame update
     void Start()
     {
 		image.sprite = sprites[0];
+        image.gameObject.SetActive(false);
        
-        // right_button = GameObject.Find("Right_Button").GetComponent<Button>();
         right_button.onClick.AddListener(RightButtonClick);
+        right_button.gameObject.SetActive(false);
 
-		// left_button = GameObject.Find("Left_Button").GetComponent<Button>();
 		left_button.onClick.AddListener(LeftButtonClick);
+        left_button.gameObject.SetActive(false);
 
         cross_button.onClick.AddListener(CrossButtonClick);
+        cross_button.gameObject.SetActive(false);
 
-        // text_arr[0] = GameObject.Find("m_Text0").GetComponent<TMP_Text>();
         text_arr[0].text = "棋谱";
+        text_arr[0].alpha = 0;
 
-        // text_arr[1] = GameObject.Find("m_Text1").GetComponent<TMP_Text>();
         text_arr[1].text = "炮二平五   <color=#000000>马</color>8进7";
         text_arr[1].alpha = 0;
 
-        // text_arr[2] = GameObject.Find("m_Text2").GetComponent<TMP_Text>();
         text_arr[2].text = "<color=#000000>马</color>二进三   車9平8";
         text_arr[2].alpha = 0;
 
-        // text_arr[3] = GameObject.Find("m_Text3").GetComponent<TMP_Text>();
         text_arr[3].text = "车一平二   <color=#000000>马</color>2进3";
         text_arr[3].alpha = 0;
 
-        gameObject.SetActive(false);//初始状态，画布不显示
+        gameObject.SetActive(true);//初始状态，画布不显示
+
+        plots.text = plot_text_arr[0].text;
+        plots.color = Color.black;
+        
+        next_button.onClick.AddListener(NextPage);
 
 
     }
@@ -106,5 +116,32 @@ public class BoardCanvas : MonoBehaviour
     public void Open()
     {
         gameObject.SetActive(true);
+    }
+
+    private void NextPage()
+    {
+        plots_index++;
+        if (plots_index > MAX_PLOTS_INDEX - 1) {
+            SecondStageInit();
+            plots_index = 0;
+        }
+        plots.text = plot_text_arr[plots_index].text;
+    }
+    
+    private void SecondStageInit()
+    {
+        //剧情结束，开启解谜阶段
+
+        image.gameObject.SetActive(true);
+        right_button.gameObject.SetActive(true);
+        left_button.gameObject.SetActive(true);
+        cross_button.gameObject.SetActive(true);
+        text_arr[0].alpha = 1;
+        text_arr[1].alpha = 1;
+        text_arr[2].alpha = 1;
+        text_arr[3].alpha = 1;
+
+        plots.gameObject.SetActive(false);
+        next_button.gameObject.SetActive(false);
     }
 }
